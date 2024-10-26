6a49f86cede322327a596fd9060065b33252d9f8
DaniT128 <a.23308051281325@cbtis128.edu.mx>
2024-10-25 19:39:44 -0600

    Add files via upload

---
+++ practica 2_archivos.py
@@ -0,0 +1,13 @@
+# Abrir el archivo en modo lectura
+f = open("practica 2_daniel_arguijo_1325.txt", "r", encoding='utf-8')
+
+# Leer el contenido del archivo
+contenido = f.read()
+
+# Imprimir el contenido
+print(contenido)
+
+# Cerrar el archivo
+f.close()
+
+
